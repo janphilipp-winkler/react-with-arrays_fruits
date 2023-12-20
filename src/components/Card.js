e3c68e54ca133283384e10667e7c8e6e44be8568
Jan-Philipp Winkler <janphilipp.winkler@gmail.com>
2023-12-20 12:47:00 +0100

    changed card css styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,16 @@
 import "./Card.css";
 
 export default function Card({ name, color }) {
-  console.log(color);
-  return (
-    <p className="card" style={{ backgroundColor: `${color} ` }}>
-      {name}
-    </p>
-  );
+  return <p className={`card card--${color}`}>{name}</p>;
 }
+
+// import "./Card.css";
+
+// export default function Card({ name, color }) {
+//   console.log(color);
+//   return (
+//     <p className="card" style={{ backgroundColor: `${color} ` }}>
+//       {name}
+//     </p>
+//   );
+// }
